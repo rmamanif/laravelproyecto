65876da8c4ffa70e8990c42cc8387dab9d7142b3
rmamanif <66333746+rmamanif@users.noreply.github.com>
2021-01-20 00:44:20 -0500

    Add files via upload

---
+++ database/migrations/2021_01_19_053504_create_libros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Titulo');
+            $table->string('Reseña');
+            $table->string('Editorial');
+            $table->string('Autor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
